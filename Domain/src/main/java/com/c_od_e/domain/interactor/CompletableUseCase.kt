d6234a0b59d464ff7069409d8c1164ddb8d11a9b
Engi2nee <engi2nee@gmail.com>
2020-03-04 15:33:13 +0300

    Added BaseUseCases

---
+++ Domain/src/main/java/com/c_od_e/domain/interactor/CompletableUseCase.kt
@@ -0,0 +1,33 @@
+package com.c_od_e.domain.interactor
+
+import com.c_od_e.domain.excuter.PostExecutionThread
+import io.reactivex.Completable
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.disposables.Disposable
+import io.reactivex.observers.DisposableCompletableObserver
+import io.reactivex.schedulers.Schedulers
+
+abstract class CompletableUseCase<in Params> constructor(
+    private val postExecutionThread: PostExecutionThread
+) {
+
+    private val disposables = CompositeDisposable()
+
+    protected abstract fun buildUseCaseCompletable(params: Params? = null): Completable
+
+    open fun execute(observer: DisposableCompletableObserver, params: Params? = null) {
+        val completable = this.buildUseCaseCompletable(params)
+            .subscribeOn(Schedulers.io())
+            .observeOn(postExecutionThread.scheduler)
+        addDisposable(completable.subscribeWith(observer))
+    }
+
+    fun addDisposable(disposable: Disposable) {
+        disposables.add(disposable)
+    }
+
+    fun dispose() {
+        if (!disposables.isDisposed) disposables.dispose()
+    }
+
+}
---
+++ Domain/src/main/java/com/c_od_e/domain/repository/ProjectsRepository.kt
@@ -0,0 +1,17 @@
+package com.c_od_e.domain.repository
+
+import com.c_od_e.domain.model.Project
+import io.reactivex.Completable
+import io.reactivex.Observable
+
+interface ProjectsRepository {
+
+    fun getProjects(): Observable<List<Project>>
+
+    fun bookmarkProject(projectId: String): Completable
+
+    fun unbookmarkProject(projectId: String): Completable
+
+    fun getBookmarkedProjects(): Observable<List<Project>>
+
+}
